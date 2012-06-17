8b4e35d937bd3ecc2f085e518a7b4a8620dc1162
hash <hash@exultant.us>
2012-06-16 17:02:40 -0700

    add license text and metadata

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,26 @@
+#===============================================================================
+# Copyright 2012 Eric Myhre <http://exultant.us>
+# 
+# This file is part of mdm <https://github.com/heavenlyhash/mdm/>.
+# 
+# PSAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
 
 import argparse;
 
+__version__ = "0.1"
+__project_url__ = "https://github.com/heavenlyhash/mdm"
 
 parser = argparse.ArgumentParser(prog="mdm");
 
