315ac613a57a1a0f78cc7350c1f282bc953cb415
hash <hash@exultant.us>
2012-06-16 20:31:41 -0700

    Implementation of `mdm release`.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Core concept is this: use git submodules in a planned and effective way, and you
 	projX/releases/			<-- SUBMODULE!  points to projX-releases.  update the version this points to every time you put a release out.  (this isn't strictly necessary, and no one using this project needs to check this submodule out in order to get work done, but it just seems reasonable to bind it here.)
 	
 	projX-releases/			<-- GIT PROJECT ROOT.  this repo contains other repos, each of which contains a snapshot of one release.
-	projX-releases/v0.1.3.git/	<-- SNEAKY REPO!  this repo only contains one commit, which contains all the files output by the build of projX v0.0.1.  it's a bare git repo with its raw data actually checked in to the projX-releases repo.  it's possible to clone this repo without cloning the entire projX-releases repo by going over raw http!
+	projX-releases/v0.1.3.git/	<-- SNEAKY REPO!  this repo only contains one commit, which contains all the files output by the build of projX v0.1.3.  it's a bare git repo with its raw data actually checked in to the projX-releases repo.  it's possible to clone this repo without cloning the entire projX-releases repo by going over raw http!
 	projX-releases/v0.2.1.git/	<-- SNEAKY REPO!  this repo also only contains one commit, same idea.
 	projX-releases/snapshots	<-- SUBMODULE!  optional pattern: you could actually have a repo here that has 'nightlies' or whathaveyou in it, and go ahead and keep advancing the artifacts in this one.  it might be handy to be able to reference this in another project's dev branch, but you'd probably never point to it for a release or stable branch just because it'd be so big.  (alternately: just lots and lots more single-commit point-release submodules, but we put them in a subdirectory to make prettier browsing of the important ones.)
 	
@@ -39,4 +39,56 @@ and that means you want to be safe and secure in case the central/blessed server
 
 Notice how everything about this system is build-tool agnostic!  You can use ant, maven, makefiles, rakefiles, hoefiles, whatever you want; the dependency storage planning
 will work the same for any kind of artifact files.
+
+None of the ideas here are particularly radical.  The key is just being organized and having a plan that leverages the existing tools out there to their full potential.
+However, admittedly, the steps involved in performing a release with this process are not completely obvious when looking at the man page for "```git submodule```",
+and even with the full pattern laid out before you, it's a lot of commands to issue in a particular sequence.  Solution?  Automate it, of course.
+
+
+
+*mdm*, Automated!
+=================
+
+"```mdm```" is a script that will guide you / automagically perform any of the steps in the lifecycle of releasing.
+It also covers updating dependency specifications when releasing.
+And finally, ```mdm``` can be used as shorthand when making a fresh clone or performing a pull that changes dependency versions
+(though these actions can also be easily performed by stringing some git commands together; ```mdm``` is just being a bit of porcelain when used this way).
+
+
+Usage: Releasing:
+-----------------
+
+```
+	mdm release --version v0.1.3 --files ./target/
+```
+
+That's about it.  Substitute in the values that make sense for you.
+
+Typically you'll want to run your build process right before the release command.
+You can do that manually if you like, but it's probably even easier to integrate it with your build system;
+that way you don't have to repeat yourself with the version number or the path to the produced files.
+With a typical Ant setup, that might look like this:
+
+```
+	<target name="release" depends="clean, dist">
+		<exec executable="mdm">
+			<arg value="release" />
+			<arg value="--version=${version}" />
+			<arg value="--files=${dist}" />
+		</exec>
+	</target>
+```
+
+Alternatively, if you're the cautious type, you may wish to perform your build, then inspect the produced files at your leisure, and then call ```mdm release``` only when you're satisfied.
+
+
+### Prerequisite Setup
+
+(There's not much, but there's a little.)
+
+When you're setting up a project, you need to not only create the git repository for that project, but also a repository for its releases.  (Hopefully this doesn't surprise you too much.)
+
+It's also typical to set up your (that is, you-as-the-release-author) project repository so that its releases repository is a submodule and set up to be writable.
+(Doing that also requires that your releases module has a ```remote/origin``` url set up, or git won't let you set up the submodule.  But you need that set up in order for anyone to refer to your releases anyway, right?  Right.)
+Of course, this is totally optional and you can refuse to do so and instead run mdm a la ```mdm release --repo=../my/weird/path/releases-repo```.
 
--- src/__main__.py
+++ src/__main__.py
@@ -17,7 +17,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
+from sys import stderr;
 import argparse;
+from pbs import git, xargs, cd, ls, cp, rm, glob;
+from pbs import ErrorReturnCode, ErrorReturnCode_1, ErrorReturnCode_2;
 
 __version__ = "0.1"
 __project_url__ = "https://github.com/heavenlyhash/mdm"
@@ -30,7 +33,7 @@ __project_url__ = "https://github.com/heavenlyhash/mdm"
 def mdm_make_argsparser():
 	parser = argparse.ArgumentParser(prog="mdm");
 	parser.add_argument("--version", action="version", version=__version__);
-	subparser = parser.add_subparsers(dest="subcommand");
+	subparser = parser.add_subparsers(dest="subcommand", title="subcommands");
 	mdm_make_argsparser_dependsc(subparser);
 	mdm_make_argsparser_releasesc(subparser);
 	mdm_make_argsparser_updatesc(subparser);
@@ -59,9 +62,25 @@ def mdm_make_argsparser_releasesc(subparser):
 		help="generates a new release (creating and committing a new bare repo into your project's releases repo, then pulling that update and commiting it into the project's releases submodule).",
 	);
 	parser_makerelease.add_argument(
+		"--version",
+		required=True,				# this appears to be broken and does not cause correct doc generation in my version of python.  so that's disappointing.
+		help="specifies the name/number of the release version to be made.  This will be used in creating tags, commit messages, directory names, and also given to the build command so that it can be included in the generated artifacts by that process.",
+	);
+	parser_makerelease.add_argument(
 		"--repo",
-		help="specifies a local path to the releases repository for this project.  The new release snapshot repo will be added as raw data to this repository.  By default, it is assumed that the releases repo of this project is already a submodule in the ./releases/ directory, but using a path like '../projX-releases/' is also reasonable.",
+		help="specifies a local path to the releases repository for this project.  The new release snapshot repo will be added as raw data to this repository.  By default, it is assumed that the releases repo of this project is already a submodule in the ./releases/ directory, but using a path like '../projX-releases/' is also reasonable.  (default: '%(default)s')",
 		default="releases",
+	);
+	group = parser_makerelease.add_mutually_exclusive_group();
+	group.add_argument(
+		"--buildcmd",
+		help="specifies build command.  The version number will placed where '{}' occurs in the command string.  The current working directory of the command will be the snapshot repo already initialized in the releases repo.  (default: '%(default)s')  Either this or --files may be used to define what goes in the release snapshot.",
+		default="ant -Dversion={} -Ddist=.",
+	);
+	group.add_argument(
+		"--files",
+		help="specifies a directory to get artifact files from.  Either this or --buildcmd may be used to define what goes in the release snapshot.",
+		# this can NOT be replicated by buildcmd just being a `cp` command (i.e. `cp -r ../../dist/* .`) because the glob wouldn't get expanded (and doing it without the glob is... not what you want).
 	);
 
 def mdm_make_argsparser_updatesc(subparser):
@@ -79,19 +98,6 @@ def mdm_make_argsparser_clonesc(subparser):
 
 
 #===============================================================================
-# main
-#===============================================================================
-args = mdm_make_argsparser().parse_args();
-print {
-	 'depend': lambda args : args.subcommand + " not yet implemented",
-	'release': lambda args : args.subcommand + " not yet implemented",
-	 'update': lambda args : args.subcommand + " not yet implemented",
-	  'clone': lambda args : args.subcommand + " not yet implemented",
-}[args.subcommand](args);
-
-
-
-#===============================================================================
 # depend
 #===============================================================================
 
@@ -103,7 +109,64 @@ print {
 # release
 #===============================================================================
 
-# TODO
+def mdm_release(args):
+	# sanity check the releases-repo
+	try: ls(args.repo);			# check that releases-repo is at least a directory
+	except ErrorReturnCode_2:
+		print >> stderr, "repo directory '"+args.repo+"' doesn't exist!\n";
+		exit(3);
+	cd(args.repo);				# enter releases-repo
+	try: ls(".git");			# check that releases-repo is already a git repo (I considered using `git status` here, but it will always false-positive because it looks up at the parent dir.)
+	except ErrorReturnCode_2:
+		print >> stderr, "repo directory '"+args.repo+"' doesn't look like a git repo!\n";
+		exit(3);
+	try:					# check that nothing is already in the place where this version will be placed
+		ls(args.version);
+		print >> stderr, "something already exists at '"+args.repo+"/"+args.version+"' !  Can't release there.\n";
+		exit(3);
+	except ErrorReturnCode_2:
+		pass;	#good
+	
+	# make the snapshot-repo
+	git.init(args.version);						# create new snapshot-repo
+	cd(args.version);						# enter snapshot-repo
+	
+	# do the build / copy in the artifacts
+	try:	# if anything fails in building, we want to destroy the snapshot area so it's not a mess the next time we try to do a release.
+		if (args.files):
+			cp(glob(args.files+"/*"), ".");				# copy in artifacts via glob (we don't really want to match dotfiles on the off chance someone considers their entire repo to be snapshot-worthy, because then we'd grab the .git files, and that would be a mess.)
+		else:
+			xargs("-I={}", "-r",					# invoke the build (via xargs, because we're using it to do the substitution of the version option.)
+				args.buildcmd.split()[:1], args.buildcmd.split()[1:],
+				_in=args.version);
+	except:
+		print >> stderr, "error during build!\n";
+		cd(".."); rm("-r", args.version);
+		raise;
+	
+	# commit the snapshot-repo
+	git.add(".");							# add the artifacts to snapshot-repo
+	git.commit("-m","release snapshot version "+args.version);	# commit the artifacts to snapshot-repo
+	git.tag(args.version);						# tag the snapshot commit in this snapshot-repo		#TODO: there should be a signing option here.
+	cd("..");							# back out to the releases-repo
+	
+	# commit the snapshot-repo into the releases-repo
+	git.clone(args.version, "--bare", args.version+".git");		# clone a bare mirror of the snapshot-repo
+	rm("-r", args.version+".git/hooks");				# remove files from the bare snapshot-repo that are junk
+	with open(args.version+".git/refs/heads/.gitignore", 'w') as f: f.write("");	# you don't wanna know.
+	git.add(args.version+".git");					# add the raw data of the bare snapshot-repo to the releases-repo
+	with open(".gitignore", 'a') as f: f.write(args.version+"\n");	# add the new snapshot-repo to the .gitignore so we can have it be expanded later without there being noise
+	git.add(".gitignore");						# and stage that .gitignore change we just made
+	git.commit("-m","release snapshot version "+args.version);	# commit the raw data of the bare snapshot-repo to the releases-repo
+	git.tag(args.version);						# tag the snapshot commit in the releases-repo		#TODO: there should be a signing option here.
+	
+	# push all the things
+	#  this we may not want to do without letting the user get off and look around, because it makes things fairly unreversable.
+	# TODO					# push the releases-repo
+	# TODO					# back out to the proj-repo
+	# TODO					# commit the new version of the release-repo submodule
+	
+	return "release version "+args.version+" complete  :D";
 
 
 
@@ -120,5 +183,18 @@ print {
 #===============================================================================
 
 # TODO
+
+
+
+#===============================================================================
+# main
+#===============================================================================
+args = mdm_make_argsparser().parse_args();
+print {
+	 'depend': lambda args : args.subcommand + " not yet implemented",
+	'release': mdm_release,
+	 'update': lambda args : args.subcommand + " not yet implemented",
+	  'clone': lambda args : args.subcommand + " not yet implemented",
+}[args.subcommand](args);
 
 
