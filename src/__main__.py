beaa48b1083f960d90c00533e49e3fdfc281bb0f
hash <hash@exultant.us>
2012-07-03 01:43:35 -0700

    Add more error checking to `mdm depend add`, and reorder some of the existing stuff report errors before wasting more of your attention than minimally necessary.

--- src/__main__.py
+++ src/__main__.py
@@ -265,6 +265,10 @@ def mdm_depend_add(args):
 	if (not isGitRepoRoot(".")):
 		return mdm_status(":(", "this command should be run from the top level folder of your git repo.");
 	
+	# git's behavior of assuming relative urls should be relative to the remote origin instead of relative to the local filesystem is almost certainly not what you want.
+	if (args.url[:3] == "../" or args.url[:2] == "./"):
+		return mdm_status(":(", "you can't use a relative url to point to a dependency.  sorry.");
+	
 	# pick out the name.  if we can't find one yet, we'll prompt for it in a little bit (we try to check that something at least exists on the far side of the url before bothering with the name part).
 	name = None;
 	if (args.name):		# well that was easy
@@ -277,6 +281,20 @@ def mdm_depend_add(args):
 			if (tehMatch):
 				name = tehMatch.group(1);
 				break;
+		# prompt for a name if we don't have one picked yet.
+		if (not name):
+			name = raw_input("dependency name: ");
+	
+	# we shall normalize a bit of uri here into the local uri.
+	#  it's rather disconcerting later if we don't (some commands will normalize it, and others won't, and that's just a mess), and we also want to check for local existance of a submodule here before getting too wild.
+	path = relpath(join(args.lib, name));
+	
+	# check for presence of a submodule or other crap here already.  (`git submodule add` will also do this, but it's a more pleasant user experience to check this before popping up a prompt for version name.)
+	#  there are actually many things you could check here: presence of files, presence of entries in .git/config, presence of entries in .gitmodules, presence of data in the index.  we're going to just use our default pattern from isSubmodule() and then check for plain files.
+	if (isSubmodule(path)):
+		return mdm_status(":(", "there is already a submodule at "+path+" !");
+	if (os.path.exists(path)):
+		return mdm_status(":(", "there are already files at "+path+" ; can't create a submodule there.");
 	
 	# if a specific version name was given, we'll skip checking for a manifest and just go straight at it; otherwise we look for a manifest and present options interactively.
 	version = None;
@@ -293,13 +311,6 @@ def mdm_depend_add(args):
 			if (not version in versions):
 				print "\""+version+"\" is not in the list of available versions; double check your typing.";
 				version = None;
-	
-	# check if we got a name earlier, and prompt for it if we don't have one picked yet.
-	if (not name):
-		name = raw_input("dependency name: ");
-	
-	# we shall normalize a bit of uri here since it's rather disconcerting later if we don't (some commands will normalize it, and others won't, and that's just a mess).
-	path = relpath(join(args.lib, name));
 	
 	# add us a submodule for great good!
 	print >> stderr, "adding "+name+"-"+version+" to "+args.lib+" from "+args.url;
