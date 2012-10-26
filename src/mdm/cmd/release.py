0c388371afb94d6e91cefc8dd7738c5215584234
hash <hash@exultant.us>
2012-10-25 20:59:49 -0700

    Fix a help message that was still referring to the old command names.

--- src/mdm/cmd/release.py
+++ src/mdm/cmd/release.py
@@ -7,7 +7,7 @@ def release(args):
 	
 	# sanity check the releases-repo
 	if (not cgw.isRepoRoot(args.repo)):	# check that releases-repo is already a git repo
-		return exitStatus(":(", "releases-repo directory '"+args.repo+"' doesn't look like a git repo!  (Maybe you forgot to set up with `mdm init` before making your first release?)");
+		return exitStatus(":(", "releases-repo directory '"+args.repo+"' doesn't look like a git repo!  (Maybe you forgot to set up with `mdm release-init` before making your first release?)");
 	cd(args.repo);				# enter releases-repo
 	git.checkout("master");			# history of the releases-repo is supposed to be linear.  things get confusing to push if they're not, and in particular we want to make sure that if there's currently a detatched head because of submodule updating leaving the releases repo in that state, we don't start racking up commits in that unpleasant void.
 	try:					# check that nothing is already in the place where this version will be placed
