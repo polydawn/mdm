6ccb4e3a5ec8eed5dba7aefac06a50a576cf839c
hash <hash@exultant.us>
2012-10-27 20:01:13 -0700

    update comment to make sense in context of branchwise.

--- src/mdm/plumbing.py
+++ src/mdm/plumbing.py
@@ -41,7 +41,7 @@ def doDependencyAdd(name, url, version):
 	git.submodule("add", join(url, version+".git"), name);				# add us a submodule for great good!
 	git.submodule("init", name);							# i would've thought `git submodule add` would have already done this, but it seems sometimes it does not.  anyway, at worst, this is a redunant no-op.
 	git.config("-f", ".gitmodules", "submodule."+name+".mdm", "dependency");	# put a marker in the submodules config that this submodule is a dependency managed by mdm.
-	# git.config("-f", ".gitmodules", "submodule."+name+".mdm-version", version);	# we could add another marker to make the version name an explicit property, but what would be the point?  our purposes are served well enough by making the pathname have an extremely explicit connection to the version name.
+	# git.config("-f", ".gitmodules", "submodule."+name+".mdm-version", version);	# we could add another marker to make the version name an explicit property, but what would be the point?  it wouldn't have any real binding to what objects are pointed at by the git index nor what's actually checked out.
 	git.add(".gitmodules");								# have to `git add` the gitmodules file again since otherwise the marker we just appended doesn't get staged
 	pass;
 
