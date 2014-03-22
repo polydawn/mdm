180d82d8b5b30921fd2e24c2dfaa1df17eb33bf4
Eric Myhre <hash@exultant.us>
2014-03-22 13:42:44 -0500

    create a helper method for listing files in a repo at a ref; seems like it's going to come up a lot in crafting tests.

--- src/test-fixtures/net/polydawn/mdm/fixture/FixtureUtil.java
+++ src/test-fixtures/net/polydawn/mdm/fixture/FixtureUtil.java
@@ -1,10 +1,13 @@
 package net.polydawn.mdm.fixture;
 
 import java.io.*;
+import java.util.*;
 import net.polydawn.mdm.*;
 import net.polydawn.mdm.commands.*;
 import org.eclipse.jgit.errors.*;
 import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.revwalk.*;
+import org.eclipse.jgit.treewalk.*;
 
 public class FixtureUtil {
 	public static Repository setUpPlainRepo(String path) throws IOException {
@@ -24,5 +27,18 @@ public class FixtureUtil {
 			.setWorkTree(new File(path).getCanonicalFile())
 			.build();
 		return releaserepo;
+	}
+
+	public static List<String> listTreePaths(Repository repo, String ref) throws IOException {
+		ObjectId commitId = repo.getRef(ref).getObjectId();
+		RevCommit revCommit = new RevWalk(repo).parseCommit(commitId);
+		TreeWalk treeWalk = new TreeWalk(repo);
+		treeWalk.setRecursive(true);
+		treeWalk.reset(revCommit.getTree());
+		List<String> answer = new ArrayList<String>();
+		while (treeWalk.next()) {
+			answer.add(treeWalk.getPathString());
+		}
+		return answer;
 	}
 }
