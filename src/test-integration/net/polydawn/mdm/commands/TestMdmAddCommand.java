d2d4500c14643702d6491d8150a9d0169e042d27
Eric Myhre <hash@exultant.us>
2014-03-22 18:22:55 -0500

    add test for MdmAddCommand against a release repo with multiple versions.  introduces fixture ProjectBetaReleases.

--- src/test-integration/net/polydawn/mdm/commands/TestMdmAddCommand.java
+++ src/test-integration/net/polydawn/mdm/commands/TestMdmAddCommand.java
@@ -73,4 +73,41 @@ public class TestMdmAddCommand extends TestCaseUsingRepository {
 			fail("adding should have failed, the requested version does not exist");
 		} catch (MdmExitMessage expected) {}
 	}
+
+	@Test
+	public void testAddFromLocalRelrepWithMultipleVersions() throws Exception {
+		Fixture project = new ProjectAlpha("projectRepo");
+		Fixture releases = new ProjectBetaReleases("projectRepo-releases");
+
+		WithCwd wd = new WithCwd(project.getRepo().getWorkTree()); {
+			MdmAddCommand cmd = new MdmAddCommand(project.getRepo());
+			cmd.url = releases.getRepo().getWorkTree().toString();
+			cmd.name = "depname";
+			cmd.pathLibs = new File("lib");
+			cmd.version = "v1.1";
+			cmd.validate();
+			assertJoy(cmd.call());
+		} wd.close();
+
+		File depPath = new File(project.getRepo().getWorkTree()+"/lib/depname").getCanonicalFile();
+
+		// i do hope there's a filesystem there now
+		assertTrue("dependency module path exists on fs", depPath.exists());
+		assertTrue("dependency module path is dir", depPath.isDirectory());
+
+		// assert on the refs in the release module we added to the project repo
+		Collection<Ref> refs = new Git(project.getRepo()).lsRemote()
+				.setRemote(depPath.toString())
+				.call();
+		List<String> refNames = new ArrayList<String>(refs.size());
+		for (Ref r : refs) refNames.add(r.getName());
+		assertTrue("head ref present in dependency module", refNames.contains("HEAD"));
+		assertTrue("release branch present in dependency module", refNames.contains("refs/heads/mdm/release/v1.1"));
+		assertTrue("release tag present in dependency module", refNames.contains("refs/tags/release/v1.1"));
+		assertEquals("exactly these three refs present in dependency module", 3, refNames.size());
+
+		// check the actual desired artifacts are inside the release module location
+		assertEquals("exactly two files exist (.git and the artifact)", 2, depPath.listFiles().length);
+		assertEquals("content of artifact is correct", "beta release 1.1", IOForge.readFileAsString(new File(depPath, "beta")));
+	}
 }
