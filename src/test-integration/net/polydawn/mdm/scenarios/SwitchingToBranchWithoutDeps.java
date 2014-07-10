576af6c6fc5bbbcc9036f94195164d16c0cec1ac
Eric Myhre <hash@exultant.us>
2014-07-10 17:53:04 -0500

    define a failing test for switching to branches that leaves a dependency unlinked, looking towards addressing this as a feature in the near future.
    currently, mdm leaves the working tree alone.  it's a situation where the config defining the desired state is versioned, and there's a tool dragging the world along behind it: absense of config is different than config for removal.  Other dependency managers have similar issues; concretely, I tried ivy once and was surprised to discover my working tree stayed rather un-gc'd (though of course usually that goes somewhere under your homedir; it's better if you don't make eye contact, right?); and I've seen blogs describing the issue for devops tools as well; basically I don't feel alone in this issue.
    
    but, i think we might be able to do something clever with dropping an owned-by-mdm flag in the git data dir of dependency submodules.  that would give us enough information to know what we can safely remove (vs what's a submodule we don't own, or what's another git repo just hanging out).  solution still needed for how to actually detect the submodule repos once on a branch that's missing the gitlink, though -- the current SubmoduleWalk code path won't cut it for that.

---
+++ src/test-integration/net/polydawn/mdm/scenarios/SwitchingToBranchWithoutDeps.java
@@ -0,0 +1,52 @@
+package net.polydawn.mdm.scenarios;
+
+import static org.junit.Assert.assertTrue;
+import java.io.*;
+import net.polydawn.josh.*;
+import net.polydawn.mdm.*;
+import net.polydawn.mdm.fixture.*;
+import net.polydawn.mdm.test.*;
+import net.polydawn.mdm.test.WithCwd;
+import org.junit.*;
+import org.junit.runner.*;
+
+@RunWith(OrderedJUnit4ClassRunner.class)
+public class SwitchingToBranchWithoutDeps extends TestCaseUsingRepository {
+	final Josh git = new Josh("git");
+
+	@Test
+	public void should_delete_unlinked_deps_on_update() throws Exception {
+		Fixture project = new ProjectAlpha("projectAlpha");
+		Fixture releases = new ProjectAlphaReleases("projectAlpha-releases");
+
+		// create a branch and link a dep on it.  (leave master with no deps.)
+		WithCwd wd = new WithCwd(project.getRepo().getWorkTree()); {
+			git.args("checkout", "-b", "with-mdm").start().get();
+			assertJoy(Mdm.run(
+				"add",
+				releases.getRepo().getWorkTree().toString(),
+				"--version=v1",
+				"--name=alpha"
+			));
+		} wd.close();
+
+		// sanity check: dep path should exist
+		File depWorkTreePath = new File(project.getRepo().getWorkTree()+"/lib/alpha").getCanonicalFile();
+		assertTrue("dependency module path should exist on fs", depWorkTreePath.exists());
+		assertTrue("dependency module path should be dir", depWorkTreePath.isDirectory());
+
+		// checkout back onto master.  update.
+		wd = new WithCwd(project.getRepo().getWorkTree()); {
+			git.args("checkout", "master").start().get();
+			assertJoy(Mdm.run("update"));
+		} wd.close();
+
+		// dep path should not exist
+		assertTrue("dependency module path should be absent on fs", !depWorkTreePath.exists());
+	}
+
+	@Test
+	public void should_leave_normal_unlinked_submodules_alone_on_update() throws Exception {
+
+	}
+}
